1b2ed20a9c576e02999483e51ba7f96e20873e14
Rabab-Muhammad <rababmuhammadhussain@gmail.com>
2024-04-27 19:26:28 +0500

    adding 100 days coding challenge day-1

---
+++ main.js
@@ -0,0 +1,13 @@
+//     100 DAYS OF COODING CHALLENGE
+//             "DAY 1"
+//Q1) Install Node.js, TypeScript and VS Code on your computer/Laptop.
+//Q2) Personal Message: Store a person’s name in a variable and print a message to them,
+//      like “Hello Asharib, would you like to learn some TypeScript today?”
+var personName = "William,";
+console.log("Hello", personName, "would you like to learn some TypeScript today?");
+//Q3) Name Cases: Store a person’s name in a variable, 
+//   and then print that person’s name in lowercase, uppercase, and titlecase.
+var person_name = "Nelson";
+console.log(person_name.toLowerCase());
+console.log(person_name.toUpperCase());
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,20 @@
+
+//     100 DAYS OF COODING CHALLENGE
+
+//             "DAY 1 challenge:start coding"
+
+//Q1) Install Node.js, TypeScript and VS Code on your computer/Laptop.
+
+//Q2) Personal Message: Store a person’s name in a variable and print a message to them,
+//      like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let personName = "William,";
+console.log("Hello",personName,"would you like to learn some TypeScript today?");
+
+//Q3) Name Cases: Store a person’s name in a variable, 
+//   and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let person_name = "Nelson";
+console.log(person_name.toLowerCase());
+console.log(person_name.toUpperCase());
+console.log(person_name.charAt(0).toUpperCase()+person_name.slice(1).toLowerCase());
